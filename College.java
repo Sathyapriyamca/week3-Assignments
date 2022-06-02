9ec9bb4fb7222b2a7d9a6eb0f8caf3211e3aa93d
Sathyapriyamca <98675170+Sathyapriyamca@users.noreply.github.com>
2022-06-02 23:31:23 +0530

    College.java

---
+++ College.java
@@ -0,0 +1,19 @@
+package org.college;
+
+public class College {
+	public void collegeName() {
+		System.out.println("Muthyammal Engineering College ");
+	}
+	public void collegeCode() {
+		System.out.println("College Code is 6216");
+	}
+	public void collegeRank() {
+		System.out.println("49");
+		
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+}
