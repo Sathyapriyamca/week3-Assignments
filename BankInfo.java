f6ddc6402bbccdea80488887e3bc9c572cb26344
Sathyapriyamca <98675170+Sathyapriyamca@users.noreply.github.com>
2022-06-02 23:33:19 +0530

    BankInfo.java

---
+++ BankInfo.java
@@ -0,0 +1,15 @@
+package bank;
+
+public class BankInfo {
+
+	public void savings() {
+		System.out.println("My Savings");
+	}
+
+	public void fixed() {
+		System.out.println("My Account");
+	}
+	public void deposit() {
+		System.out.println("My Balance");
+	}
+}
