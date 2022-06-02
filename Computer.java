a9948e0afa848c7a1f10a9a70e49d8f5fb4a1c4c
Sathyapriyamca <98675170+Sathyapriyamca@users.noreply.github.com>
2022-06-02 23:28:12 +0530

    Computer.java

---
+++ Computer.java
@@ -0,0 +1,19 @@
+package org.system;
+
+public class Computer {
+	public void ComputerModel()
+	
+	{
+		System.out.println("My Computer Model is Dell");
+
+	}
+
+	public static void main(String[] args){
+	
+		Computer comp=new Computer();
+		comp.ComputerModel();
+	}
+}
+
+		
+			
